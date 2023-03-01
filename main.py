a2fe3b094261a742c9a20bec20509a68d5378041
bitwopi <donmormajor@gmail.com>
2023-03-02 02:25:28 +0300

    add some logic

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ from exercise import Exercise
 from preset import Preset
 
 
+# Containers
 class PresetListContainer(GridLayout):
     def __init__(self, **kwargs):
         super(PresetListContainer, self).__init__(**kwargs)
         for item in MainApp.get_running_app().presets:
-            self.add_widget(PresetBoxLayout(item.name))
+            self.add_widget(PresetBoxLayout(item))
         self.cols = 1
         self.hint_size_y = None
         self. spacing = 10
@@ -24,44 +25,62 @@ class PresetListContainer(GridLayout):
 
 
 class ExerciseListContainer(GridLayout):
-    def __init__(self, **kwargs):
+    preset = None
+
+    def __init__(self, *args, **kwargs):
         super(ExerciseListContainer, self).__init__(**kwargs)
         app = MainApp.get_running_app()
-        if app.current_preset is not None:
-            for item in app.current_preset.exercises:
+        if len(args) > 0:
+            self.preset = args[0]
+        elif app.current_preset is not None:
+            self.preset = app.current_preset
+        if self.preset is not None:
+            for item in self.preset.exercises:
                 self.add_widget(PresetBoxLayout(item))
         self.cols = 1
         self.hint_size_y = None
-        self. spacing = 10
+        self.spacing = 10
         self.padding = 10
         self.height = self.minimum_height
 
 
+# BoxLayouts
 class PresetBoxLayout(BoxLayout):
     def __init__(self, *args, **kwargs):
         super(PresetBoxLayout, self).__init__(**kwargs)
-        self.add_widget(Label(text=args[0]))
+        self.preset = args[0]
+        self.add_widget(Label(text=self.preset.name))
         self.size_hint = (1, None)
         self.height = 130
 
+    def set_preset(self):
+        MainApp.get_running_app().current_exercise = self.preset
+
 
 class ExerciseBoxLayout(BoxLayout):
+
     def __init__(self, *args, **kwargs):
         super(ExerciseBoxLayout, self).__init__(**kwargs)
         self.exercise = args[0]
         self.add_widget(Label(text=self.exercise.name))
-
         self.size_hint = (1, None)
         self.height = 130
 
+    def set_exercise(self):
+        MainApp.get_running_app().current_exercise = self.exercise
 
+
+# Screens
 class StartingScreen(Screen):
     pass
 
 
 class EditPresetScreen(Screen):
     preset_name = ObjectProperty(None)
+    sv = ObjectProperty(None)
     preset_exercises = []
+    preset_exercises_btns = []
+    last_exercise = None
 
     def __init__(self, *args, **kwargs):
         super(EditPresetScreen, self).__init__(**kwargs)
@@ -73,11 +92,22 @@ class EditPresetScreen(Screen):
         else:
             self.preset_name.text = 'new_preset'
 
+    def on_pre_enter(self, *args):
+        app = MainApp.get_running_app()
+        if self.last_exercise is not None:
+            ex_id = self.preset_exercises.index(self.last_exercise)
+            self.preset_exercises[ex_id] = app.current_exercise
+        else:
+            self.preset_exercises.append(app.current_exercise)
+
     def save(self):
         MainApp.get_running_app().save_preset(Preset(self.preset_name.text, self.preset_exercises))
 
+    def add(self):
+        self.last_exercise = MainApp.get_running_app().current_exercise
+
     def delete(self):
-        pass
+        self.preset_exercises.remove(MainApp.get_running_app().current_exercise)
 
 
 class EditExerciseScreen(Screen):
@@ -110,6 +140,29 @@ class EditExerciseScreen(Screen):
             self.exercise_brk.bind(text=self.format_time)
             self.exercise_brk.bind(text=self.format_time)
 
+    def build_exercise(self):
+        try:
+            brk = int(self.clear_s(self.exercise_brk.text))
+            if self.toggle_button.state == "normal":
+                time = int(self.clear_s(self.exercise_time.text))
+                reps = None
+            else:
+                reps = int(self.exercise_reps.text)
+                time = None
+
+            return Exercise(name=self.exercise_name.text,
+                            brk=brk,
+                            time=time,
+                            reps=reps
+                            )
+        except:
+            return None
+
+    def save(self):
+        exercise = self.build_exercise()
+        if exercise is not None:
+            MainApp.get_running_app().current_exercise = self.build_exercise()
+
     def on_toggle_button_state(self, button):
         if button.state == "normal":
             button.text = "exercise duration"
@@ -121,6 +174,7 @@ class EditExerciseScreen(Screen):
             self.exercise_time.disabled = True
 
     def format_time(self, instance, value: str):
+        """ formatting time fields and allow to spell only numbers """
         try:
             new_val = self.clear_s(value)
             print(new_val)
@@ -130,6 +184,7 @@ class EditExerciseScreen(Screen):
             instance.text = "s"
 
     def clear_s(self, string):
+        """ remove all 's' characters in string """
         return string.replace("s", "")
 
 
@@ -138,6 +193,7 @@ class TrainingScreen(Screen):
     pass
 
 
+# App
 class MainApp(App):
     presets = []
     current_preset = None
@@ -158,9 +214,11 @@ class MainApp(App):
 
     def save_preset(self, new_preset: Preset):
         if self.current_preset is not None:
-            self.presets.remove(self.current_preset)
+            p_id = self.presets.index(self.current_preset)
             self.current_preset = None
-        self.presets.append(new_preset)
+            self.presets[p_id] = new_preset
+        else:
+            self.presets.append(new_preset)
 
 
 if __name__ == '__main__':
