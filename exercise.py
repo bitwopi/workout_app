fe53393a2835088ee655dc8958a98439b8835d1f
bitwopi <donmormajor@gmail.com>
2023-03-05 17:58:39 +0300

    recycle views are working + data usage algorithms almost ready

--- exercise.py
+++ exercise.py
@@ -2,16 +2,12 @@ class Exercise:
     def __init__(self, **kwargs):
         self.name = kwargs['name']
         self.brk = kwargs['brk']
-        if 'time' in kwargs:
-            self.time = kwargs['time']
-            self.reps = None
-        else:
-            self.reps = kwargs['pers']
-            self.time = None
+        self.time = kwargs['time']
+        self.reps = kwargs['reps']
 
     def __str__(self):
         if self.reps is not None:
-            return
+            return f"{self.name}({self.reps} reps)"
         else:
             return f"{self.name}({self.time}s)"
 
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.recycleview import RecycleView
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.uix.recycleboxlayout import RecycleBoxLayout
 from kivy.uix.behaviors import FocusBehavior
@@ -13,6 +14,10 @@ from kivy.properties import ObjectProperty, BooleanProperty
 
 from exercise import Exercise
 from preset import Preset
+
+
+class RV(RecycleView):
+    list = ObjectProperty(None)
 
 
 class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior, RecycleBoxLayout):
@@ -43,18 +48,23 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
         ''' Respond to the selection of items in the view. '''
         self.selected = is_selected
         if self.selected:
-            MainApp.get_running_app().current_exercise = self.index
-
+            app = MainApp.get_running_app()
+            if app.root.current == 'edit_preset':
+                self.parent.parent.parent.on_select()
+            if isinstance(self.parent.parent.parent, StartingScreen):
+                MainApp.get_running_app().current_preset = self.obj
 
 
 # Screens
 class StartingScreen(Screen):
-    pass
+    preset_list = ObjectProperty(None)
+
+    def on_enter(self, *args):
+        self.preset_list.data = [{'text': item.name, 'obj': item} for item in MainApp.get_running_app().presets]
 
 
 class EditPresetScreen(Screen):
     preset_name = ObjectProperty(None)
-    sv = ObjectProperty(None)
     grid = ObjectProperty(None)
     preset_exercises = ObjectProperty(None)
     exercise_name = ObjectProperty(None)
@@ -65,85 +75,83 @@ class EditPresetScreen(Screen):
 
     def __init__(self, **kwargs):
         super(EditPresetScreen, self).__init__(**kwargs)
+        self.exercise_time.bind(text=self.format_time)
         self.exercise_brk.bind(text=self.format_time)
-        self.exercise_brk.bind(text=self.format_time)
+        self.exercise_reps.bind(text=self.format_int)
+        self.toggle_button.state = "down"
 
     def on_pre_enter(self, *args):
         app = MainApp.get_running_app()
         if app.current_preset is not None:
             self.preset_name.text = app.current_preset.name
-            labels = []
-            for item in app.current_preset.exercises:
-                if item.reps is not None:
-                    labels.append(f"{item.name}({item.reps} reps) with {item.brk}s break")
-                else:
-                    labels.append(f"{item.name}({item.time}s) with {item.brk}s break")
+            labels = [f"{item.__str__()} with {item.brk}s break" for item in app.current_preset.exercises]
             self.preset_exercises.data = [{'text': text, 'obj': obj}
                                           for text, obj in zip(labels, app.current_preset.exercises)]
         else:
             self.preset_name.text = 'new_preset'
             self.preset_exercises.data = []
 
-    def on_touch_down(self, touch):
-        if len(self.preset_exercises.selected) > 0:
+    def on_select(self):
+        if len(self.preset_exercises.list.selected_nodes) > 0:
             app = MainApp.get_running_app()
-            selected_ex = self.preset_exercises.selected[0]
-            if selected_ex.index != app.current_exercise_index:
-                self.exercise_name.text = selected_ex.obj.name
-                self.exercise_brk.text = f"{selected_ex.obj.brk}s"
-                if selected_ex.obj.time is not None:
-                    self.exercise_time.text = f"{selected_ex.obj.time}s"
-                    self.exercise_reps.text = "0"
-                    self.exercise_reps.disabled = True
-                    self.toggle_button.state = "normal"
-                else:
-                    self.exercise_time.text = str(selected_ex.obj.reps)
-                    self.exercise_time.text = "0s"
-                    self.exercise_time.disabled = True
-                    self.toggle_button.state = "down"
+            selected_ex = self.preset_exercises.data[self.preset_exercises.list.selected_nodes[0]]
+            print(selected_ex.obj)
+            self.exercise_name.text = selected_ex.obj.name
+            self.exercise_brk.text = f"{selected_ex.obj.brk}s"
+            if selected_ex.obj.time is not None:
+                self.exercise_time.text = f"{selected_ex.obj.time}s"
+                self.exercise_reps.text = "0"
+                self.exercise_reps.disabled = True
+                self.toggle_button.state = "down"
+            else:
+                self.exercise_time.text = str(selected_ex.obj.reps)
+                self.exercise_time.text = "0s"
+                self.exercise_time.disabled = True
+                self.toggle_button.state = "normal"
 
     def on_leave(self, *args):
         app = MainApp.get_running_app()
         app.current_exercise_index = None
         app.current_preset = None
+        self.clear_fields()
 
     def save_exercise(self):
-        if len(self.preset_exercises.selected) > 0:
-            selected_ex = self.preset_exercises.selected[0]
-            selected_ex.obj = self.build_exercise()
-            if selected_ex.obj.time is None:
-                selected_ex.text = f"{selected_ex.obj.name}({selected_ex.obj.time}s)  with {selected_ex.obj.brk}s break)"
-            else:
-                selected_ex.text = f"{selected_ex.obj.name}({selected_ex.obj.time} reps)  with {selected_ex.obj.brk}s " \
-                                   f"break)"
-        else:
+        if len(self.preset_exercises.list.selected_nodes) > 0:
+            index = self.preset_exercises.list.selected_nodes[0]
             new_ex = self.build_exercise()
-            if new_ex.time is None:
-                text = f"{new_ex.name}({new_ex.time}s)  with {new_ex.brk}s break)"
-            else:
-                text = f"{new_ex.name}({new_ex.reps} reps)  with {new_ex.brk}s break)"
+            self.preset_exercises.data[index] = {'text': f"{new_ex.__str__()} with {new_ex.brk}s break"}
+        elif self.build_exercise() is not None:
+            new_ex = self.build_exercise()
+            text = f"{new_ex.__str__()}  with {new_ex.brk}s break"
             self.preset_exercises.data.append({'text': text, 'obj': new_ex})
-        self.preset_exercises.clear_selection()
+        self.clear_fields()
         MainApp.get_running_app().current_exercise_index = None
 
     def save(self):
         MainApp.get_running_app().save_preset(Preset(self.preset_name.text, self.get_data()))
 
     def add(self):
-        self.preset_exercises.clear_selection()
+        self.preset_exercises.list.clear_selection()
         MainApp.get_running_app().current_exercise_index = None
         self.exercise_name.text = "new_exercise"
         self.exercise_brk.text = "30s"
         self.exercise_time.text = "15s"
         self.toggle_button.state = "down"
         self.exercise_reps.text = "15"
-        self.exercise_time.disabled = True
+
+    def clear_fields(self):
+        self.preset_exercises.list.clear_selection()
+        MainApp.get_running_app().current_exercise_index = None
+        self.exercise_name.text = ""
+        self.exercise_brk.text = ""
+        self.exercise_time.text = ""
+        self.toggle_button.state = "down"
+        self.exercise_reps.text = ""
 
     def delete(self):
-        if len(self.preset_exercises.selected) > 0:
-            self.preset_exercises.data.remove(self.preset_exercises.selected[0])
-            self.preset_exercises.clear_selection()
-            MainApp.get_running_app().current_exercise_index = None
+        if len(self.preset_exercises.list.selected_nodes) > 0:
+            self.preset_exercises.data.remove(self.preset_exercises.data[self.preset_exercises.list.selected_nodes[0]])
+            self.clear_fields()
 
     def on_toggle_button_state(self, button):
         if button.state == "normal":
@@ -156,21 +164,18 @@ class EditPresetScreen(Screen):
             self.exercise_time.disabled = True
 
     def get_data(self):
-        return [item.obj for item in self.preset_exercises.data]
+        return [item['obj'] for item in self.preset_exercises.data]
 
     def format_time(self, instance, value: str):
         """ formatting time fields and allow to spell only numbers """
-        try:
-            new_val = self.clear_s(value)
-            print(new_val)
-            int(new_val)
-            instance.text = new_val + "s"
-        except:
-            instance.text = "s"
+        instance.text = self.clear_s(value) + "s"
+
+    def format_int(self, instance, value):
+        instance.text = self.clear_s(value)
 
     def clear_s(self, string):
         """ remove all 's' characters in string """
-        return string.replace("s", "")
+        return ''.join([char for char in string if char.isdigit()])
 
     def build_exercise(self):
         """ build new exercise object depending on text inputs values """
@@ -182,13 +187,14 @@ class EditPresetScreen(Screen):
             else:
                 reps = int(self.exercise_reps.text)
                 time = None
-
+            print(brk, time, reps)
             return Exercise(name=self.exercise_name.text,
                             brk=brk,
                             time=time,
                             reps=reps
                             )
-        except:
+        except Exception as ex:
+            print(ex)
             return None
 
 
@@ -201,7 +207,6 @@ class TrainingScreen(Screen):
 class MainApp(App):
     presets = []
     current_preset = None
-    current_exercise_index = None
 
     def build(self):
         sm = ScreenManager()
@@ -220,7 +225,6 @@ class MainApp(App):
             p_id = self.presets.index(self.current_preset)
             self.current_preset = None
             self.presets[p_id] = new_preset
-            self.temp_preset = None
         else:
             self.presets.append(new_preset)
 
